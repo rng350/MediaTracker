0bdb6501d89bb33dff545e9941a07f0208f889f0
Robert <rnguyen350@gmail.com>
2025-01-28 14:44:43 -0500

    text display in bottom navigation bar items

--- app/src/main/java/com/rng350/mediatracker/screens/main/MediaTrackerBottomAppBar.kt
+++ app/src/main/java/com/rng350/mediatracker/screens/main/MediaTrackerBottomAppBar.kt
@@ -3,6 +3,7 @@ package com.rng350.mediatracker.screens.main
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -23,21 +24,21 @@ fun MediaTrackerBottomAppBar(
             selected = currentTab.value == BottomTab.Discover,
             onClick = { onTabClicked(BottomTab.Discover) },
             icon = { Icon(imageVector = ImageVector.vectorResource(R.drawable.internet_24px), contentDescription = "Discover") },
-            label = { stringResource(R.string.discover) },
+            label = { Text(text = stringResource(R.string.discover)) },
             alwaysShowLabel = true
         )
         NavigationBarItem(
             selected = currentTab.value == BottomTab.Watchlist,
             onClick = { onTabClicked(BottomTab.Watchlist) },
             icon = { Icon(imageVector = ImageVector.vectorResource(R.drawable.schedule_24px_unfilled), contentDescription = "Watchlist") },
-            label = { stringResource(R.string.movie_watchlist) },
+            label = { Text(text=stringResource(R.string.movie_watchlist)) },
             alwaysShowLabel = true
         )
         NavigationBarItem(
             selected = currentTab.value == BottomTab.Watched,
             onClick = { onTabClicked(BottomTab.Watched) },
             icon = { Icon(imageVector = ImageVector.vectorResource(R.drawable.visibility_24px_unfilled), contentDescription = "Watched") },
-            label = { stringResource(R.string.watched_movies) },
+            label = { Text(text=stringResource(R.string.watched_movies)) },
             alwaysShowLabel = true
         )
     }
