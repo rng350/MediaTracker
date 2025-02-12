305ecfd770119314a49bb866162fd8b85469c45c
Robert <rnguyen350@gmail.com>
2025-02-12 13:24:28 -0500

    prevent get movie details screen from crashing when trying to access while offline

--- app/src/main/java/com/rng350/mediatracker/movies/usecases/FetchMovieDetailsRemotelyUseCase.kt
+++ app/src/main/java/com/rng350/mediatracker/movies/usecases/FetchMovieDetailsRemotelyUseCase.kt
@@ -1,5 +1,6 @@
 package com.rng350.mediatracker.movies.usecases
 
+import android.util.Log
 import com.rng350.mediatracker.common.database.MovieDao
 import com.rng350.mediatracker.movies.MovieDetails
 import com.rng350.mediatracker.networking.TMDBApi
@@ -7,6 +8,8 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import javax.inject.Inject
 import okio.IOException
+import retrofit2.HttpException
+import kotlin.coroutines.cancellation.CancellationException
 
 class FetchMovieDetailsRemotelyUseCase @Inject constructor(
     private val tmdbApi: TMDBApi,
@@ -15,22 +18,43 @@ class FetchMovieDetailsRemotelyUseCase @Inject constructor(
     suspend operator fun invoke(movieId: String): MovieDetails? = coroutineScope {
         return@coroutineScope try {
             val movieDetailsFetched = async {
-                tmdbApi.getMovieDetails(movieId).body()?.toMovieDetailsForDisplay()
+                try {
+                    tmdbApi.getMovieDetails(movieId).body()?.toMovieDetailsForDisplay()
+                }
+                catch (e: IOException) {
+                    Log.e("FetchMovieDetailsRemotelyUseCase", "Network error while trying to remotely fetch movie details: ${e.localizedMessage}")
+                    null
+                }
             }
             val movieUserStatusFetched = async {
-                movieDao.getMovieUserStatus(movieId.toInt())
+                try {
+                    movieDao.getMovieUserStatus(movieId.toInt())
+                }
+                catch (e: Exception) {
+                    Log.e("FetchMovieDetailsRemotelyUseCase", "Database error: ${e.localizedMessage}")
+                    null
+                }
             }
             val movieDetails = movieDetailsFetched.await()
             val movieUserStatus = movieUserStatusFetched.await()
             movieDetails?.copy(
-                isLiked = movieUserStatus.isLiked,
-                isOnWatchlist = movieUserStatus.isInWatchlist,
-                hasBeenWatched = movieUserStatus.hasBeenWatched
+                isLiked = movieUserStatus?.isLiked ?: false,
+                isOnWatchlist = movieUserStatus?.isInWatchlist ?: false,
+                hasBeenWatched = movieUserStatus?.hasBeenWatched ?: false
             )
         }
-        // Network connectivity issue
-        catch(e: IOException) {
-            null
+        catch (e: HttpException) {
+            Log.e("FetchMovieDetailsRemotelyUseCase", "HTTP error: ${e.code()}")
+            return@coroutineScope null
+        } catch (e: IOException) {
+            Log.e("FetchMovieDetailsRemotelyUseCase", "Network error :(((( : ${e.localizedMessage}")
+            return@coroutineScope null
+        } catch(e: CancellationException) {
+            Log.e("FetchMovieDetailsRemotelyUseCase", "CancellationException -- Parent coroutine was cancelled:. MESSAGE: ${e.message ?: "No message"}")
+            return@coroutineScope null
+        } catch (e: Exception) {
+            Log.e("FetchMovieDetailsRemotelyUseCase", "Unexpected error: ${e.localizedMessage}")
+            return@coroutineScope null
         }
     }
 }
